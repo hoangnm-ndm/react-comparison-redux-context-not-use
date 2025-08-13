4ad58ae060c54c889591add358bb0639f996b37f
hoangnm-ndm <hoangnm.ndm@gmail.com>
2025-08-13 15:32:49 +0700

    multi reducer

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,27 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { RootState, AppDispatch } from "./store";
 import { increment, decrement } from "./features/counter/counterSlice";
+import { addTodo } from "./features/todo/todoSlice";
 
 const App: React.FC = () => {
 	const count = useSelector((state: RootState) => state.counter.count);
 	const dispatch = useDispatch<AppDispatch>();
+
+	const todo = useSelector((state: RootState) => state.todo.todos);
+	console.log(todo);
 	return (
 		<div>
 			<h2>Count: {count}</h2>
 			<button onClick={() => dispatch(increment())}>Tăng</button>
 			<button onClick={() => dispatch(decrement())}>Giảm</button>
+			<button onClick={() => dispatch(addTodo({ id: 1, text: "Them viec can lam" }))}>them viec can lam</button>
+
+			<h2>Danh sach viec can lam</h2>
+			<div>
+				{todo.map((item) => (
+					<div key={item.id}>{item.text}</div>
+				))}
+			</div>
 		</div>
 	);
 };
